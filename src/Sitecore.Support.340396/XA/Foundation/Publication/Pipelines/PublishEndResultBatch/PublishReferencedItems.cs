6c69428e38ef37e768df6e0387ccc9a19fe1f92b
DK\KIV <kirill.volovitskiy@sitecore.net>
2019-07-01 17:00:43 +0300

    Publish related item only if the corresponding checkbox is selected

---
+++ src/Sitecore.Support.340396/XA/Foundation/Publication/Pipelines/PublishEndResultBatch/PublishReferencedItems.cs
@@ -0,0 +1,26 @@
+﻿using Sitecore.Publishing.Service.Pipelines.BulkPublishingEnd;
+using System;
+
+namespace Sitecore.Support.XA.Foundation.Publication.Pipelines.PublishEndResultBatch
+{
+    public class PublishReferencedItems : Sitecore.XA.Foundation.Publication.Pipelines.PublishEndResultBatch.
+        PublishReferencedItems
+    {
+        public PublishReferencedItems(
+            Sitecore.XA.Foundation.SitecoreExtensions.Repositories.IDatabaseRepository databaseRepository) : base(
+            databaseRepository)
+        {
+        }
+
+        public new void Process(PublishEndResultBatchArgs args)
+        {
+            if (args == null || args.JobData == null || args.JobData.Metadata == null ||
+                string.IsNullOrEmpty(args.JobData.Metadata["Publish.Options.IncludeRelatedItems"]) ||
+                args.JobData.Metadata["Publish.Options.IncludeRelatedItems"].Equals("false", StringComparison.OrdinalIgnoreCase) ||
+                args.JobData.PublishType == null ||
+                !args.JobData.PublishType.Equals("Single Item", StringComparison.OrdinalIgnoreCase))
+                return;
+            base.Process(args);
+        }
+    }
+}
