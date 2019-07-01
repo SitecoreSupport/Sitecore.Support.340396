c5246c9bef29356e2b62a02367df1f8dbbec4b25
DK\KIV <kirill.volovitskiy@sitecore.net>
2019-07-01 16:59:56 +0300

    Pathch infrastructure preparation

---
+++ src/Sitecore.Support.340396/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.340396")]
+[assembly: AssemblyProduct("Sitecore.Support.340396")]
+[assembly: ComVisible(false)]
